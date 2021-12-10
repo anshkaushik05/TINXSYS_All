6a7c94c2f9a227a86cf807b558b922d8ba254f0e
AnshKaushik <anshkaushik05@gmail.com>
2021-12-10 09:41:47 +0530

    form validation table structured and styled

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors :{
+        primaryTable: '#2C3034' ,
+        secondaryTable: '#323539',
+    },
+    },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
